631816b111d808598faacd6ebbfa63951c9d22e2
mwhorse46 <needmefordev@gmail.com>
2022-07-22 20:13:54 +0600

    added day one and two practice

---
+++ day-1/info.js
@@ -0,0 +1,43 @@
+
+
+
+// Day-1 
+// 20 July 2022
+
+
+// Day-1
+// —---------
+// 1.Watch Orientation class video (video link)
+// 2.Pick your action plan with proper mindset and time management plan 
+// 3.Post your Goal  in webdeveloperbD community as described in orientation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day-2/script.js
@@ -0,0 +1,54 @@
+
+
+
+
+// JavaScript Essential for React.js
+// var, let, const
+
+
+
+// function statement
+// statement => command
+
+// function expression
+// expression => value
+
+// arrow function
+
+// template string
+
+
+// conditionals
+// ternary
+// logical and operator (&&)
+// logical or operator (||)
+// short circuit
+
+
+// array helper method (map, filter, reduce)
+// imperative coding
+// declarative coding
+
+
+
+// destructuring, rest, spread
+
+
+// promise 
+// fetch
+// async / await
+
+// class
+// inheritance
+
+
+
+
+
+
+
+
+
+
+
+
