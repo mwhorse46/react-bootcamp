e8286e3354879d931bd8927efd373e4f347fc705
mwhorse46 <needmefordev@gmail.com>
2022-07-27 08:40:19 +0600

    added vite project and minor change

--- day-2/script.js
+++ day-2/script.js
@@ -39,6 +39,7 @@
 
 
 // class
+// encapsulation
 // inheritance
 // abstraction
 // polymorphism
--- day-4/react.js
+++ day-4/react.js
@@ -10,18 +10,31 @@
 
 
 
+// create-react-app
+// combine multiple files -> webpack, parcel
+// processing next generation javascript -> babel
+// code quality check -> eslint
 
 
 
 
 
+// for (var i = 0; i < 10; i++) {
+//     setTimeout(() => console.log(i), 0);
+// }
 
 
+//   নিচের 'b' অবজেক্ট এর ভিতরের 'a' প্রোপার্টিটির ভ্যালু destructure করে কিভাবে নিরাপদ ভাবে প্রিন্ট করবেন? মনে রাখবেন 'c' অবজেক্টেটি নিজে সহ তার যেকোন প্রোপার্টি মিসিং থাকতে পারে।
 
-
-
-
-
+//   const a = 5;
+//   const b = {
+//       b: 1,
+//       c: {
+//           d: {
+//               a: 5,
+//           },
+//       },
+//   };
 
 
 
