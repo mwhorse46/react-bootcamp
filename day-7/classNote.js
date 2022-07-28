5151beeea85faffbdc56a5b19a0df52c2c736ad4
mwhorse46 <needmefordev@gmail.com>
2022-07-28 09:38:01 +0600

    aded missing days and day-7

---
+++ day-7/classNote.js
@@ -0,0 +1,54 @@
+
+
+
+
+/************************************************************************************************************
+ *                                          Day-7 class note
+ ************************************************************************************************************/
+
+// synthetic event
+
+
+// when component re-render?
+// state update
+// force update
+// props change
+
+
+// jsx rules
+// wrapped parent element by using div or react fragment
+// must use closing tag
+// must be careful of javascript reserved keyword
+// dynamic value written in curly braces{}
+// dynamic value must be expression
+
+
+// css style
+// inline css
+// external css
+// module css
+// component based css
+
+
+
+
+
+
+
+
+
+const fullName = 'fName'
+
+const obj = {[fullName]: 'Kawsar Ahmed'};
+
+
+
+
+
+
+
+
+
+
+
+
--- day-7/my-app/src/App.js
+++ day-7/my-app/src/App.js
@@ -1,16 +1,16 @@
 import { useState } from 'react';
-
-
+import styles from './App.module.css';
 
 function App() {
 
-
-
   const [count, setCount] = useState(0)
-  
   const handleIncrement = (num) => setCount(count + num);
   const handleDecrement = (num) => setCount(prevCount => prevCount - num)
   const handleReset = () => setCount(0);
+
+  const randomValues = [1, 3, 6, 7, 9, 10];
+  const [pickedValue, setPickedValue] = useState(null);
+
 
   
   return (
@@ -20,9 +20,52 @@ function App() {
       <button onClick={() => handleIncrement(10)}>Increment</button>
       <button onClick={() => handleDecrement(10)}>Decrement</button>
       <button onClick={handleReset}>Reset</button>
+      <br />
+      <br />
+      <IsOddOrEven value={pickedValue} />
+      <RandomCard randomValues={randomValues} setPickedValue={setPickedValue} />
     </>
     
   );
+}
+
+
+function IsOddOrEven({value}) {
+
+  const oddOrEven = value % 2 === 0 ? 'even' : 'odd'
+
+  return (
+    <>
+      {/* <div>Number is {value && value % 2 === 0 ? <span style={{color:'green'}}>Even</span> : <span style={{color:'red'}}>Odd</span>}</div> */}
+      <br />
+
+      <div>
+        Number is <span className={ oddOrEven === 'even' ? styles.even : styles.odd}>{oddOrEven}</span>
+      </div>
+      <br />
+    </>
+  )
+}
+
+
+function RandomCard({randomValues, setPickedValue}) {
+  
+  return (
+    <div>
+      {
+        randomValues.map((randomValue, index) => <div style={{
+          display: 'inline-block',
+          width: '150px',
+          height: '150px',
+          borderRadius: '50%',
+          backgroundColor: 'red',
+          textAlign: 'center',
+          lineHeight: '150px',
+          color: 'white'
+        }} key={index} onClick={() => setPickedValue(randomValue)}>{randomValue}</div>)
+      }
+    </div>
+  )
 }
 
 export default App;
