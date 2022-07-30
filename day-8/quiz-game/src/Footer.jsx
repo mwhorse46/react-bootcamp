91a6921460a3a707010bc8d729430533c904b1f8
mwhorse46 <needmefordev@gmail.com>
2022-07-30 22:39:20 +0600

    reduce button width

--- day-8/quiz-game/src/Footer.jsx
+++ day-8/quiz-game/src/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div style={{textAlign: 'center', backgroundColor: '#fab1a0', height: '10vh', margin: 'auto 0', lineHeight: '10vh'}}>
+        <div style={{ backgroundColor: '#fab1a0', height: '10vh', margin: 'auto 0', display: 'flex', alignItems: 'center', justifyContent: 'center', textAlign: 'center'}}>
             <h4 style={{margin: 0}}>Developed by <a target="__blank" href="https://web.facebook.com/web.kawsarahmed">Kawsar Ahmed</a> || Initiative with Web Developer BD</h4>
         </div>
     );
