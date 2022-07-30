a0080c5ed3fcb771f84b8bcbed3a827d1f9cbf6d
mwhorse46 <needmefordev@gmail.com>
2022-07-30 22:45:06 +0600

    minor change footer font

--- day-8/quiz-game/src/Footer.jsx
+++ day-8/quiz-game/src/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div style={{ backgroundColor: '#fab1a0', height: '10vh', margin: 'auto 0', display: 'flex', alignItems: 'center', justifyContent: 'center', textAlign: 'center'}}>
-            <h4 style={{margin: 0}}>Developed by <a target="__blank" href="https://web.facebook.com/web.kawsarahmed">Kawsar Ahmed</a> || Initiative with Web Developer BD</h4>
+            <h5 style={{margin: 0}}>Developed by <a target="__blank" href="https://web.facebook.com/web.kawsarahmed">Kawsar Ahmed</a> || Initiative with Web Developer BD</h5>
         </div>
     );
 };
