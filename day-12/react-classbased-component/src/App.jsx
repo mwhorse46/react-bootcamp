130c0207fa1ec49cbea25ac5588b90b04281d61b
mwhorse46 <needmefordev@gmail.com>
2022-08-02 16:26:21 +0600

    added class based componenet 1st way

---
+++ day-12/react-classbased-component/src/App.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  // 1st way
+  // state = {count: 0}
+  // handleIncrement = () => {
+  //   this.setState({count: this.state.count + 1});
+  // }
+  // handleDecrement = () => {
+  //   this.setState({count: this.state.count - 1});
+  // }
+  // handleReset = () => {
+  //   this.setState({count: 0});
+  // }
+
+
+  
+
+
+
+  render() {
+
+
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    );
+  }
+}
+
+export default App;
