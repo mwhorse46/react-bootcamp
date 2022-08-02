6dab27f9ac16d5e56e5b44732f95793c1ff3bb6e
mwhorse46 <needmefordev@gmail.com>
2022-08-02 17:48:19 +0600

    added class componnent 1st way and 2nd way

--- day-12/react-classbased-component/src/App.jsx
+++ day-12/react-classbased-component/src/App.jsx
@@ -4,30 +4,61 @@ class App extends Component {
 
   // 1st way
   // state = {count: 0}
-  // handleIncrement = () => {
+  // handleIncrement() {
   //   this.setState({count: this.state.count + 1});
   // }
-  // handleDecrement = () => {
+  // handleDecrement() {
   //   this.setState({count: this.state.count - 1});
   // }
-  // handleReset = () => {
+  // handleReset() {
   //   this.setState({count: 0});
   // }
 
 
-  
+
+
+  // 2nd way
+  state = {count: 0}
+  handleIncrement = () => {
+    this.setState({count: this.state.count + 1});
+  }
+  handleDecrement = () => {
+    this.setState({count: this.state.count - 1});
+  }
+  handleReset = () => {
+    this.setState({count: 0});
+  }
+
+
+
+
+
+ 
+
+
+
 
 
 
   render() {
 
-
+  
     return (
       <div>
+
+        {/* <h1>{this.state.count}</h1>
+        <button onClick={this.handleIncrement.bind(this)}>Increment</button>
+        <button onClick={this.handleDecrement.bind(this)}>Decrement</button>
+        <button onClick={this.handleReset.bind(this)}>Reset</button> */}
+
+
         <h1>{this.state.count}</h1>
         <button onClick={this.handleIncrement}>Increment</button>
         <button onClick={this.handleDecrement}>Decrement</button>
         <button onClick={this.handleReset}>Reset</button>
+
+
+
       </div>
     );
   }
