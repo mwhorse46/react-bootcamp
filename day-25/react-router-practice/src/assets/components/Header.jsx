1b76c4b06da172a831c3fd27af0149541a35066b
mwhorse46 <needmefordev@gmail.com>
2022-08-14 21:20:23 +0600

    added navlink and style

--- day-25/react-router-practice/src/assets/components/Header.jsx
+++ day-25/react-router-practice/src/assets/components/Header.jsx
@@ -1,45 +1,63 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
 
 
 
 const Header = () => {
-    return (
-        <>
-            <Navbar bg="light" expand="md">
-                <Container>
-                    <Navbar.Brand href="/">Navbar scroll</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                    <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                    >
-                        <Link className='nav-link' to="/home">Home</Link>
-                        <Link className='nav-link' to="/about">About</Link>
-                        <Link className='nav-link' to="/contact" >
-                            Contact
-                        </Link>
-                    </Nav>
-                    <Form className="d-flex">
-                        <Form.Control
-                        type="search"
-                        placeholder="Search"
-                        className="me-2"
-                        aria-label="Search"
-                        />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
-    );
+  let activeStyle = {
+    textDecoration: "underline",
+    color: 'black'
+  };
+
+  return (
+    <>
+      <Navbar bg="light" expand="md">
+        <Container>
+          <Navbar.Brand href="/">Navbar scroll</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="me-auto my-2 my-lg-0">
+              <NavLink
+                style={({isActive}) => (isActive ? activeStyle : undefined)}
+                className="nav-link"
+                to="/home"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                style={({isActive}) => (isActive ? activeStyle : undefined)}
+                className="nav-link"
+                to="/about"
+              >
+                About
+              </NavLink>
+              <NavLink
+                style={({isActive}) => (isActive ? activeStyle : undefined)}
+                className="nav-link"
+                to="/contact"
+              >
+                Contact
+              </NavLink>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 
 export default Header;
