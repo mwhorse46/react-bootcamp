e6b1f9006e60f495f70938fbb1bc8bf05d57d4ae
mwhorse46 <needmefordev@gmail.com>
2022-08-15 21:04:47 +0600

    added usenavigate feathure

--- day-25/react-router-practice/src/assets/components/Founder.jsx
+++ day-25/react-router-practice/src/assets/components/Founder.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 
 
 const Founder = () => {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate('/home');
+    }, [])
     return (
         <div>
             <h2>This is Founder Component</h2>
