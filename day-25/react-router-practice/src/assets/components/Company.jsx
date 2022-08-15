17c31203c234e04994b0ead966810c92c60da0cf
mwhorse46 <needmefordev@gmail.com>
2022-08-15 21:08:38 +0600

    addd use location

--- day-25/react-router-practice/src/assets/components/Company.jsx
+++ day-25/react-router-practice/src/assets/components/Company.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 
 
 
 
 const Company = () => {
     const {name} = useParams();
-
-
+    const location = useLocation();
+    console.log(location, 'location');
     
     return (
         <div>
