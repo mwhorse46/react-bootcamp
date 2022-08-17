5cdb18dae95b9e9966a758061e6322d45b5d79e3
mwhorse46 <needmefordev@gmail.com>
2022-08-17 20:49:25 +0600

    added reducer full way

--- day-28/global-state-concepts/src/App.jsx
+++ day-28/global-state-concepts/src/App.jsx
@@ -1,26 +1,67 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
+  const initialState = 0;
 
-  // const [count, dispatch] = useReducer()
+  const INCREMENT = 'INCREMENT';
+  const DECREMENT = 'DECREMENT';
+  const RESET = 'RESET';
+
+  // action creator
+  const countIncrement = (dispatch, num) => {
+    return dispatch({ type: INCREMENT, payload: num});
+  }
+
+  const countReducer = (state, action) => {
+
+    // if(action.type === INCREMENT) {
+    //   return state + action.payload;
+    // } else {
+    //   return state;
+    // }
+
+    const {type, payload} = action;
+    switch (type) {
+
+      case INCREMENT:
+        return state + payload;
+
+      case DECREMENT:
+        return state - payload;
+
+      case RESET:
+        return payload;
+
+      default:
+        return state;
+    }
+
+  }
+
+  const [count, dispatch] = useReducer(countReducer, initialState)
 
 
   const handleIncrement = () => {
 
-    setCount((prevCount) => prevCount + 1);
+    // setCount((prevCount) => prevCount + 1);
+    // countIncrement(dispatch, 10);
+
+    dispatch({ type: INCREMENT, payload: 5});
   }
 
   const handleDecrement = () => {
 
-    setCount((prevCount) => prevCount - 1);
+    // setCount((prevCount) => prevCount - 1);
+    dispatch({ type: DECREMENT, payload: 5});
   }
 
 
   const handleReset = () => {
 
-    setCount(0);
+    // setCount(0);
+    dispatch({ type: RESET, payload: 0});
   }
   
 
