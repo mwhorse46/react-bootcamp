26b2ff54bc988dde7d45fe48c127443147128561
mwhorse46 <needmefordev@gmail.com>
2022-08-18 11:57:14 +0600

    practice context in counter app by destructure way

--- day-28/global-state-concepts/src/App.jsx
+++ day-28/global-state-concepts/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { createContext } from 'react';
+import React, { createContext, useReducer } from 'react';
 import Counter from './components/Counter';
 
 
@@ -7,15 +7,86 @@ import Counter from './components/Counter';
 export const CounterContext = createContext();
 
 const App = () => {
-  
 
+
+    // const [count, setCount] = useState(0);
+
+    const INCREMENT = 'INCREMENT';
+    const DECREMENT = 'DECREMENT';
+    const RESET = 'RESET';
+    
+    const countReducer = (state, action) => {
+
+      // if(action.type === INCREMENT) {
+      //   return state + action.payload;
+      // } else {
+      //   return state;
+      // }
+
+      const {type, payload} = action;
+      switch (type) {
+
+      case INCREMENT:
+          return state + payload;
+
+      case DECREMENT:
+          return state - payload;
+
+      case RESET:
+          return payload;
+
+      default:
+          return state;
+      }
+
+  }
+
+  const initialState = 0;
+  const [count, dispatch] = useReducer(countReducer, initialState);
+
+
+
+  // action creator
+  const countIncrement = (dispatch, num) => {
+      return dispatch({ type: INCREMENT, payload: num});
+  }
+
+
+
+
+
+
+  const handleIncrement = () => {
+
+      // setCount((prevCount) => prevCount + 1);
+      // countIncrement(dispatch, 10);
+
+      dispatch({ type: INCREMENT, payload: 10});
+  }
+
+  const handleDecrement = () => {
+
+      // setCount((prevCount) => prevCount - 1);
+      dispatch({ type: DECREMENT, payload: 5});
+  }
+
+
+  const handleReset = () => {
+
+      // setCount(0);
+      dispatch({ type: RESET, payload: 0});
+  }
+
+
+
+
+
+  
   const value = {
-    arr: [1,3, 5, 7, 9],
-    obj: {
-      fName: 'Kawsar',
-      lName: 'Ahmed',
-      profession: 'Developer'
-    }
+    count,
+    handleIncrement,
+    handleDecrement,
+    handleReset
   }
 
   
--- day-28/global-state-concepts/src/components/Counter.jsx
+++ day-28/global-state-concepts/src/components/Counter.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useReducer } from 'react';
+import React, { useContext } from 'react';
 import { CounterContext } from '../App';
 
 
@@ -7,74 +7,8 @@ import { CounterContext } from '../App';
 
 const Counter = () => {
 
-    // const [count, setCount] = useState(0);
-
-    const initialState = 0;
-
-    const INCREMENT = 'INCREMENT';
-    const DECREMENT = 'DECREMENT';
-    const RESET = 'RESET';
-
-    // action creator
-    const countIncrement = (dispatch, num) => {
-        return dispatch({ type: INCREMENT, payload: num});
-    }
-
-    const countReducer = (state, action) => {
-
-        // if(action.type === INCREMENT) {
-        //   return state + action.payload;
-        // } else {
-        //   return state;
-        // }
-
-        const {type, payload} = action;
-        switch (type) {
-
-        case INCREMENT:
-            return state + payload;
-
-        case DECREMENT:
-            return state - payload;
-
-        case RESET:
-            return payload;
-
-        default:
-            return state;
-        }
-
-    }
-
-    const [count, dispatch] = useReducer(countReducer, initialState);
-    const {arr, obj} = useContext(CounterContext);
-    console.log(arr, 'arr');
-    console.log(obj, 'obj');
-
-
-    const handleIncrement = () => {
-
-        // setCount((prevCount) => prevCount + 1);
-        // countIncrement(dispatch, 10);
-
-        dispatch({ type: INCREMENT, payload: 10});
-    }
-
-    const handleDecrement = () => {
-
-        // setCount((prevCount) => prevCount - 1);
-        dispatch({ type: DECREMENT, payload: 5});
-    }
-
-
-    const handleReset = () => {
-
-        // setCount(0);
-        dispatch({ type: RESET, payload: 0});
-    }
+    const {count, handleIncrement, handleDecrement, handleReset} = useContext(CounterContext);
     
-
-
 
 
     return (
